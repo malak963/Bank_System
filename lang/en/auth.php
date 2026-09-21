8ef48b51e989db3fa25bfd1ad68cfb014e5e4f66
AmalAlalwi <amalalalawi91@gmail.com>
2026-09-21 09:07:39 +0300

    Add Arabic and English translations for Fortify and 2FA

--- lang/en/auth.php
+++ lang/en/auth.php
@@ -4,4 +4,6 @@ return [
     'failed' => 'These credentials do not match our records.',
     'password' => 'The provided password is incorrect.',
     'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'two_factor_invalid' => 'The provided two factor authentication code was invalid.',
+    'two_factor_recovery_invalid' => 'The provided two factor recovery code was invalid.',
 ];
